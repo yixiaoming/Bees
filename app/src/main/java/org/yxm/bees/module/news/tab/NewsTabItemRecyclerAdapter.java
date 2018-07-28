d6811cfcba4498982e6277a53cee7745e64fd04e
yixiaoming <676893998@qq.com>
2018-07-28 14:11:08 +0800

    添加大图模板，展示福利tab

--- app/src/main/java/org/yxm/bees/module/news/tab/NewsTabItemRecyclerAdapter.java
+++ app/src/main/java/org/yxm/bees/module/news/tab/NewsTabItemRecyclerAdapter.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import org.yxm.bees.R;
 import org.yxm.bees.base.GlideApp;
@@ -30,14 +28,11 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
 
     public NewsTabItemRecyclerAdapter(List<GankEntity> datas) {
         this.mDatas = datas;
-        this.mOnItemClickListener = new OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                String url = mDatas.get(position).url;
-                Intent intent = new Intent(mContext, WebViewActivity.class);
-                intent.putExtra(WebViewActivity.PARAM_URL, url);
-                mContext.startActivity(intent);
-            }
+        this.mOnItemClickListener = position -> {
+            String url = mDatas.get(position).url;
+            Intent intent = new Intent(mContext, WebViewActivity.class);
+            intent.putExtra(WebViewActivity.PARAM_URL, url);
+            mContext.startActivity(intent);
         };
     }
 
@@ -61,6 +56,11 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
                     .inflate(R.layout.gank_recycler_item_view_three_img, parent, false);
             view.setOnClickListener(this);
             return new NewsTabItemRecyclerAdapter.ViewHolderThreeImg(view);
+        } else if (viewType == ItemType.big_image.ordinal()) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.gank_recycler_item_view_big_img, parent, false);
+            view.setOnClickListener(this);
+            return new NewsTabItemRecyclerAdapter.ViewHolderBigImg(view);
         } else {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.gank_recycler_item_item_view_no_img, parent, false);
@@ -79,6 +79,8 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
             bindOneImgViewHolder((ViewHolderOneImg) holder, position);
         } else if (holder instanceof ViewHolderThreeImg) {
             bindThreeImgViewHolder((ViewHolderThreeImg) holder, position);
+        } else if (holder instanceof ViewHolderBigImg) {
+            bindBigImgViewHolder((ViewHolderBigImg) holder, position);
         }
     }
 
@@ -95,10 +97,8 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
         holder.mAuthor.setText(item.who);
         holder.mDate.setText(item.publishedAt.substring(0, 10));
 
-        GlideApp.with(mContext).clear(holder.mPhoto);
         GlideApp.with(holder.mPhoto.getContext())
                 .load(item.images.get(0))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.mPhoto);
     }
 
@@ -108,23 +108,27 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
         holder.mAuthor.setText(item.who);
         holder.mDate.setText(item.publishedAt.substring(0, 10));
 
-        GlideApp.with(mContext).clear(holder.mImg1);
         GlideApp.with(holder.mImg1.getContext())
                 .load(item.images.get(0))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.mImg1);
 
-        GlideApp.with(mContext).clear(holder.mImg2);
         GlideApp.with(holder.mImg2.getContext())
                 .load(item.images.get(1))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.mImg2);
 
-        GlideApp.with(mContext).clear(holder.mImg3);
         GlideApp.with(holder.mImg3.getContext())
                 .load(item.images.get(2))
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.mImg3);
+    }
+
+    private void bindBigImgViewHolder(ViewHolderBigImg holder, int position) {
+        GankEntity item = mDatas.get(position);
+        holder.mAuthor.setText(item.who);
+        holder.mDate.setText(item.publishedAt.substring(0, 10));
+
+        GlideApp.with(holder.mPhoto.getContext())
+                .load(item.url)
+                .into(holder.mPhoto);
     }
 
 
@@ -132,12 +136,17 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
         no_image,
         one_image,
         three_image,
+        big_image,
         error_type;
     }
 
     @Override
     public int getItemViewType(int position) {
         GankEntity entity = mDatas.get(position);
+        if (TextUtils.equals(entity.type, "福利")) {
+            return ItemType.big_image.ordinal();
+        }
+
         if (entity.images == null || entity.images.size() == 0) {
             return ItemType.no_image.ordinal();
         } else if (entity.images.size() < 3) {
@@ -211,6 +220,20 @@ public class NewsTabItemRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
             mDate = itemView.findViewById(R.id.blog_date_text);
         }
 
+    }
+
+    private static class ViewHolderBigImg extends RecyclerView.ViewHolder{
+
+        public ImageView mPhoto;
+        public TextView mAuthor;
+        public TextView mDate;
+
+        public ViewHolderBigImg(View itemView) {
+            super(itemView);
+            mPhoto = itemView.findViewById(R.id.blog_photo);
+            mAuthor = itemView.findViewById(R.id.blog_author_text);
+            mDate = itemView.findViewById(R.id.blog_date_text);
+        }
     }
 
     /**
