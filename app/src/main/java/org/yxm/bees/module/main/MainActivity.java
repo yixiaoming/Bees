62bed0aa302e30147388714db1f801c23f060c2c
yixiaoming <676893998@qq.com>
2018-08-11 22:05:28 +0800

    调整主题，添加底部4tab的fragment切换动画

--- app/src/main/java/org/yxm/bees/module/main/MainActivity.java
+++ app/src/main/java/org/yxm/bees/module/main/MainActivity.java
@@ -1,5 +1,6 @@
 package org.yxm.bees.module.main;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
@@ -15,6 +16,9 @@ import org.yxm.bees.module.music.MusicFragment;
 import org.yxm.bees.module.personal.PersonalFragment;
 import org.yxm.bees.util.BottomNavigationViewHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
 
 public class MainActivity extends BaseMvpActivity
@@ -28,6 +32,15 @@ public class MainActivity extends BaseMvpActivity
     public static final String TAG_FRAGMENT_MUSIC = "tag_fragment_music";
     public static final String TAG_FRAGMENT_PERSONAL = "tag_fragment_personal";
 
+    private static List<String> mFragmetnIndex = new ArrayList<>(4);
+
+    static {
+        mFragmetnIndex.add(TAG_FRAGMENT_NEWS);
+        mFragmetnIndex.add(TAG_FRAGMENT_VIDEO);
+        mFragmetnIndex.add(TAG_FRAGMENT_MUSIC);
+        mFragmetnIndex.add(TAG_FRAGMENT_PERSONAL);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +51,18 @@ public class MainActivity extends BaseMvpActivity
 
     private void initViews() {
         mBottomNavView = findViewById(R.id.main_bottom_nav_bar);
+        // 设置选中颜色
+        int[][] states = new int[][]{
+                new int[]{-android.R.attr.state_checked},
+                new int[]{android.R.attr.state_checked}
+        };
+        int[] colors = new int[]{getResources().getColor(R.color.bottombar_text_color),
+                getResources().getColor(R.color.bottombar_text_checked_color)
+        };
+        ColorStateList csl = new ColorStateList(states, colors);
+        mBottomNavView.setItemTextColor(csl);
+        mBottomNavView.setItemIconTintList(csl);
+
         BottomNavigationViewHelper.disableShiftMode(mBottomNavView);
         mBottomNavView.setOnNavigationItemSelectedListener(
                 item -> {
@@ -66,7 +91,27 @@ public class MainActivity extends BaseMvpActivity
             return;
         }
         Fragment fragment = getSupportFragmentManager().findFragmentByTag(fragmentTag);
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (mCurrentFragment != null) {
+            String curTag = mCurrentFragment.getTag();
+            int curIndex = mFragmetnIndex.indexOf(curTag);
+            int nextIndex = mFragmetnIndex.indexOf(fragmentTag);
+            if (curIndex > nextIndex) {
+                transaction.setCustomAnimations(
+                        R.anim.slide_left_in,
+                        R.anim.slide_right_out,
+                        R.anim.slide_right_in,
+                        R.anim.slide_left_out);
+            } else if (curIndex < nextIndex) {
+                transaction.setCustomAnimations(
+                        R.anim.slide_right_in,
+                        R.anim.slide_left_out,
+                        R.anim.slide_left_in,
+                        R.anim.slide_right_out);
+            }
+        }
+
         if (fragment == null) {
             if (fragmentTag.equals(TAG_FRAGMENT_NEWS)) {
                 fragment = GankFragment.newInstance();
@@ -82,9 +127,11 @@ public class MainActivity extends BaseMvpActivity
                 transaction.add(R.id.main_content_framelayout, fragment, fragmentTag);
             }
         }
+
         if (mCurrentFragment != null) {
             transaction.hide(mCurrentFragment);
         }
+
         transaction.show(fragment);
         mCurrentFragment = fragment;
         transaction.commit();
