0b12e12beafa571a10fb5cb68adb91b100826205
yixiaoming <676893998@qq.com>
2018-08-12 23:49:16 +0800

    添加启动splash

--- app/src/main/java/org/yxm/bees/module/main/MainActivity.java
+++ app/src/main/java/org/yxm/bees/module/main/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends BaseMvpActivity
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
 
