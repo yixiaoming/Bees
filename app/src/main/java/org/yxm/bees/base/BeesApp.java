6067d0e38a6e1b1fc383711403d56718f9225b64
yixiaoming <yixiaoming@baidu.com>
2019-05-04 11:41:39 +0800

    删除dagger实例代码

--- app/src/main/java/org/yxm/bees/base/BeesApp.java
+++ app/src/main/java/org/yxm/bees/base/BeesApp.java
@@ -3,10 +3,6 @@ package org.yxm.bees.base;
 import android.app.Application;
 import android.content.Context;
 
-import com.google.gson.Gson;
-import javax.inject.Inject;
-import org.yxm.bees.module.main.DaggerMainActivityComponent;
-import org.yxm.bees.module.main.MainActivityComponent;
 import org.yxm.components.runtime.AppRuntime;
 import org.yxm.lib.volley.VolleyManager;
 
@@ -18,28 +14,18 @@ public class BeesApp extends Application {
 
   private static BeesApp instance;
 
-  static MainActivityComponent mainActivityComponent;
-
-  @Inject
-  Gson gson;
-
   @Override
   protected void attachBaseContext(Context base) {
     super.attachBaseContext(base);
     instance = this;
 
+    // 初始化AppRuntime，给底层模块提供app context
     AppRuntime.init(instance);
-
-    mainActivityComponent = DaggerMainActivityComponent.create();
 
     VolleyManager.init(this);
   }
 
   public static BeesApp getInstance() {
     return instance;
-  }
-
-  public static MainActivityComponent getComponent() {
-    return mainActivityComponent;
   }
 }
--- app/src/main/java/org/yxm/bees/ioc/Engine.java
+++
@@ -1,6 +0,0 @@
-package org.yxm.bees.ioc;
-
-public abstract class Engine {
-
-  public abstract void work();
-}
